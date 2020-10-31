3a38b08401544b6a8b3288c69e92bddc6a38c614
gwerrebr <gillis-wer@hotmail.be>
2020-10-31 17:21:13 +0100

    Add new solution to lab 6

--- lab06/Pakjesbezorger/src/main.cpp
+++ lab06/Pakjesbezorger/src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
 	
 	std::vector<int> voertuigen_grootte = {1,17,18,75};
-	std::vector<int> v = {75,18,17,4,1,21,86,90,38,115,1515,1516,1517}; 
+	std::vector<int> v = {75,18,17,4,1,21,86,90,38,115,1515,1516,1517};
 	for(int aant: v){ 
 		std::cout << "aantal voertuigen nodig voor " << aant << " pakjes: " << min_aantal_voertuigen(aant, voertuigen_grootte) << std::endl; 
 	} 
--- lab06/Pakjesbezorger/src/pakjes.cpp
+++ lab06/Pakjesbezorger/src/pakjes.cpp
@@ -3,47 +3,84 @@
 
 using namespace std;
 
-void min_aantal_voertuigen_recursief(int aantalPakjes, const vector<int>& voertuigen_grootte, vector<int>& currResult, vector<vector<int>>& results, int& currMin) {
-	if (aantalPakjes == 0) {
-		currMin = currResult.size();
-		results.push_back(currResult);
-		return;
-	}
+// void min_aantal_voertuigen_recursief(int aantalPakjes, const vector<int>& voertuigen_grootte, vector<int>& currResult, vector<int>& result) {
+// 	if (aantalPakjes == 0) {
+// 		result = currResult;
+// 		return;
+// 	}
 
-	auto it = voertuigen_grootte.end();
-	while (it != voertuigen_grootte.begin() && currMin > currResult.size()) {
-		it--;
+// 	auto it = voertuigen_grootte.end();
+// 	while (it != voertuigen_grootte.begin() && (result.size() == 0 ^ result.size() > currResult.size())) {
+// 		it--;
 
-		if (currResult.size() != 0 && currResult[currResult.size()-1] < *it) continue;
+// 		if (currResult.size() != 0 && currResult[currResult.size()-1] < *it) continue;
 
-		if (aantalPakjes >= *it) {
-			// add to result
-			currResult.push_back(*it);
+// 		if (aantalPakjes >= *it) {
+// 			// add to result
+// 			currResult.push_back(*it);
 
-			// recursive call
-			min_aantal_voertuigen_recursief(aantalPakjes - *it, voertuigen_grootte, currResult, results, currMin);
+// 			// recursive call
+// 			min_aantal_voertuigen_recursief(aantalPakjes - *it, voertuigen_grootte, currResult, result);
 
-			// remove result
-			currResult.pop_back();
-		}
-	}
-}
+// 			// remove result
+// 			currResult.pop_back();
+// 		}
+// 	}
+// }
+
+// int min_aantal_voertuigen(int aantalPakjes, const std::vector<int>& voertuigen_grootte) {
+// 	if (voertuigen_grootte.size() == 0) return -1;
+
+// 	vector<int> currResult;
+// 	vector<int> result;
+// 	min_aantal_voertuigen_recursief(aantalPakjes, voertuigen_grootte, currResult, result);
+
+// 	return result.size();
+// }
+
 
 int min_aantal_voertuigen(int aantalPakjes, const std::vector<int>& voertuigen_grootte) {
 	if (voertuigen_grootte.size() == 0) return -1;
 
-	vector<int> currResult;
-	vector<vector<int>> results;
-	int currMin = INT16_MAX;
-	min_aantal_voertuigen_recursief(aantalPakjes, voertuigen_grootte, currResult, results, currMin);
+	vector<vector<int>> rooster(voertuigen_grootte.size(), vector<int>(aantalPakjes+1, 0));
 
-	auto it = results.begin();
-	int optimalCount = INT16_MAX;
-	while (it != results.end()) {
-		if (it->size() < optimalCount)
-			optimalCount = it->size();		
-		it++;
+	for (int p = 1; p <= aantalPakjes; p++) {
+		int minVoertuigen = -1;
+		int huidigVoertuigen = 0;
+		vector<int> minVoertuigenV(voertuigen_grootte.size(), 0);
+		vector<int> huidigMinVoertuigenV(voertuigen_grootte.size(), 0);
+
+		for (int voertuigIndex = voertuigen_grootte.size()-1; 0 <= voertuigIndex; voertuigIndex--) {
+			if (p < voertuigen_grootte[voertuigIndex]) continue;
+
+			huidigMinVoertuigenV = vector<int>(voertuigen_grootte.size(), 0);
+
+			huidigVoertuigen = 1;
+			huidigMinVoertuigenV[voertuigIndex] = 1;
+			int restP = p - voertuigen_grootte[voertuigIndex];
+
+			if (restP != 0) {
+				for (int v = 0; v < voertuigen_grootte.size(); v++) {
+					huidigVoertuigen += rooster[v][restP];
+					huidigMinVoertuigenV[v] += rooster[v][restP];
+				}
+			}
+
+			if (minVoertuigen == -1 || minVoertuigen > huidigVoertuigen) {
+				minVoertuigen = huidigVoertuigen;
+				minVoertuigenV = huidigMinVoertuigenV;
+			}
+		}
+
+		for (int v = 0; v < voertuigen_grootte.size(); v++) {
+			rooster[v][p] = minVoertuigenV[v];
+		}
 	}
 
-	return optimalCount;
+	int aantalVoertuigen = 0;
+	for (int v = 0; v < voertuigen_grootte.size(); v++) {
+		aantalVoertuigen += rooster[v][aantalPakjes];
+	}
+
+	return aantalVoertuigen;
 }
